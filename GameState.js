1b248d7dfd6585f03a41bb08e0cc94933dba1c22
Lucas M. Schwambach <48414967+lucas427@users.noreply.github.com>
2020-08-29 18:18:56 -0300

    renamed an important variable, fixed a few comments

--- GameState.js
+++ GameState.js
@@ -20,8 +20,8 @@ class GameState {
 
 	constructor(height, width) {
 		this.matrix = new GameMatrix(height, width);
-		/* movingBlock: position of the block currently guided by the player */
-		this.movingBlock = { x:null, y:null }; 
+		/* playerBlock: position of the block currently guided by the player */
+		this.playerBlock = { x:null, y:null }; 
 		this.listeningInput = false;
 		this.gameOver = false;
 
@@ -29,9 +29,9 @@ class GameState {
 	}
 
 
-	/* newMovingBlock: generates a position on the top,
+	/* newPlayerBlock: generates a position on the top,
 	   random horizontally, for a new block for the player to move */
-	newMovingBlock() {
+	newPlayerBlock() {
 		return {
 			y: this.matrix.height - 1,
 			x: randomIntBetween(0, this.matrix.width),
@@ -46,91 +46,88 @@ class GameState {
 	}
 
 	/* newBlockRoutine: creates a new block on the top,
-	   enables player to control it by turning on listeningInput */
+	   enables player to control it by turning on listeningInput,
+	   possible game over */
 	newBlockRoutine() {
-		this.movingBlock = this.newMovingBlock();
-		if (!this.matrix.isAvailablePosition(this.movingBlock)) {
+		this.playerBlock = this.newPlayerBlock();
+		if (!this.matrix.isAvailablePosition(this.playerBlock)) {
 			this.gameOverRoutine();
 			return;
 		}
-		this.matrix.setBlock(this.movingBlock, randomBlock());
+		this.matrix.setBlock(this.playerBlock, randomBlock());
 		this.listeningInput = true;
 	}
 
 
-	/* handleInput: handles using input, assuming it matches what
-	   updateMovingBlock uses as direction for the block.
-	   Basically calls updateMovingBlock or not depending on whether
+	/* handleInput: handles user input, assuming it matches what
+	   updatePlayerBlock uses as direction for the block.
+	   Basically calls updatePlayerBlock or not depending on whether
 	   listeningInput is turned on or off. */
 	handleInput (input) {
 		if (this.listeningInput)
-			this.updateMovingBlock(input);
+			this.updatePlayerBlock(input);
 	}
 
 
-	/* updatedPosition: gives an updated position
-	   of the block the player is guiding according to the direction.
+	/* updatedPosition: gives an updated position of the block the player is
+	   guiding, according to the given direction.
 	   There are three possible directions: down+left, just down, down+right.
-	   When down+left or down+right is impossible (against a wall),
-	   the resulting direction is just down.
-	   When all directions are impossible, returns the same position
+	   When down+left or down+right is impossible (against a wall), the
+	   resulting direction is just down.
+	   When all directions are impossible, it just returns the same position
 	   as the current one. */
 	updatedPosition(direction) {
 		const yMovement = -1;
 		const xMovement = directionToXOffset[direction];
 		let updated = {
-			y: this.movingBlock.y + yMovement,
-			x: this.movingBlock.x + xMovement,
+			y: this.playerBlock.y + yMovement,
+			x: this.playerBlock.x + xMovement,
 		};
 
-		//can't move down+(left|right) (against a wall) -> just move down
+		//can't move down+(left|right) (is against a wall) -> just move down
 		if (xMovement != 0 && !this.matrix.isAvailablePosition(updated))
-			updated.x = this.movingBlock.x; //same x as before
+			updated.x = this.playerBlock.x; //same x as before, only goes down
 
-		//can't move at all -> don't
+		//can't move at all
 		if (!this.matrix.isAvailablePosition(updated))
-			updated = this.movingBlock; //same position as before
+			updated = this.playerBlock; //same position as before
 
 		return updated;
 	}
 
 
-	/* updateMovingBlock: moves the block tue player is currently
-	   guiding according to the given direction.
+	/* updatePlayerBlock: moves, according to the given direction,
+	   the block the player is currently guiding.
 	   Should be called only by handleInput().
 	   Calls movingBlockSettledRoutine() when the block reached ground. */
-	updateMovingBlock(direction) {
+	updatePlayerBlock(direction) {
 		const updated = this.updatedPosition(direction);
 
-		this.movingBlock = this.matrix.moveBlock(
-			this.movingBlock, updated
+		this.playerBlock = this.matrix.moveBlock(
+			this.playerBlock, updated
 		);
 
-		if (this.movingBlock.y == 0) //reached ground -> block settled
-			this.movingBlockSettledRoutine();
+		if (this.playerBlock.y == 0) //reached ground -> block settled
+			this.playerBlockSettledRoutine();
 		
 		const blockBelow = this.matrix.getBlock({
-			y: this.movingBlock.y - 1,
-			x: this.movingBlock.x,
+			y: this.playerBlock.y - 1,
+			x: this.playerBlock.x,
 		});
 		if (blockBelow != ' ') //on top of another block -> block settled
-			this.movingBlockSettledRoutine();
+			this.playerBlockSettledRoutine();
 	}
 
 
-	/* movingBlockSettledRoutine: does what is necessary
+	/* playerBlockSettledRoutine: does what is necessary
 	   once the block guided by the player is settled: evaluate
 	   and update the matrix until the game can continue by
 	   creating a new block */
-	movingBlockSettledRoutine() {
+	playerBlockSettledRoutine() {
 		this.listeningInput = false;
 
 		// ...
 
 		this.newBlockRoutine();
 	}
-
-
-
-
 }
