71cf59b907d2d9e62536e2a51e438ef9a7c2555f
Mateus Brandt <mateus.cruz@effecti.com.br>
2020-09-07 17:49:14 -0300

    initial styling

--- Game.js
+++ Game.js
@@ -4,25 +4,22 @@
  * block-logic.js (in createNewPlayerBlock)
  */
 
+/* should be updated to match what the keyboard listener returns */
 const directionToXOffset = {
-	ArrowLeft:  -1,
-	ArrowRight:  1,
-	ArrowDown:   0,
+	ArrowLeft: -1,
+	ArrowRight: 1,
+	ArrowDown: 0,
 };
 
-
 class Game {
-
 	constructor(height, width) {
 		this.matrix = new Matrix(height, width);
-		this.playerBlock = { x:null, y:null };
+		this.playerBlock = { x: null, y: null };
 		/* playerBlock: points the block currently guided by the player in the matrix */
 	}
 
-
 	isPlayerBlockSettled() {
-		if (this.playerBlock.y == 0)
-			return true;
+		if (this.playerBlock.y == 0) return true;
 		const blockBelow = {
 			y: this.playerBlock.y - 1,
 			x: this.playerBlock.x,
@@ -30,18 +27,16 @@ class Game {
 		return !this.matrix.isAvailable(blockBelow);
 	}
 
-
 	isGameOver() {
 		const y = this.matrix.height - 1;
 		for (let x = 0; x < this.matrix.width; x++) {
-			const settled = !this.matrix.isAvailable({x, y})
-			             && !this.matrix.isAvailable({x, y:y-1});
-			if (settled)
-				return true;
+			const settled =
+				!this.matrix.isAvailable({ x, y }) &&
+				!this.matrix.isAvailable({ x, y: y - 1 });
+			if (settled) return true;
 		}
 		return false;
 	}
-
 
 	/* createNewPlayerBlock: instantiates a new block on the top
 	 * of the matrix, which this.playerBlock will point to */
@@ -52,7 +47,6 @@ class Game {
 		};
 		this.matrix.setBlock(this.playerBlock, randomBlock());
 	}
-
 
 	/* updatePlayerBlock: moves the playerBlock according
 	 * to the given direction */
@@ -84,28 +78,21 @@ class Game {
 		 * if it can't, then tries to move just horizontally.
 		 * if it can't, then tries to move just down.
 		 * if it can't, then doesn't move at all */
-		if (this.matrix.isAvailable(horizontalDown))
-			updated = horizontalDown;
-		else if (this.matrix.isAvailable(horizontal))
-			updated = horizontal;
-		else if (this.matrix.isAvailable(down))
-			updated = down;
-		else
-			updated = original;
+		if (this.matrix.isAvailable(horizontalDown)) updated = horizontalDown;
+		else if (this.matrix.isAvailable(horizontal)) updated = horizontal;
+		else if (this.matrix.isAvailable(down)) updated = down;
+		else updated = original;
 
 		/* moves the block in the matrix and updates this.playerBlock to point to it */
 		this.playerBlock = this.matrix.moveBlock(this.playerBlock, updated);
 	}
 
-
 	start() {
 		this.createNewPlayerBlock();
 	}
 
-
 	update(input) {
-		if (this.isGameOver())
-			return;
+		if (this.isGameOver()) return;
 
 		this.updatePlayerBlock(input);
 		if (!this.isGameOver() && this.isPlayerBlockSettled()) {
@@ -114,28 +101,28 @@ class Game {
 		}
 	}
 
-
 	/* HTMLrendering: returns a rendering of the matrix as an HTML table.
 	 * styling is defered, as only the css classes are provided */
-	HTMLrendering() {
-		let html = '';
+	HTMLrendering(gameOverModal) {
+		let html = "";
 		let block;
 		let danger; //to make the top row red, indicating game over
 		html += '<table id="game-table">';
-		for (let y = this.matrix.height-1; y >= 0; y--) {
-			danger = y == this.matrix.height-1 ? 'danger' : '';
+		for (let y = this.matrix.height - 1; y >= 0; y--) {
+			danger = y == this.matrix.height - 1 ? "danger" : "";
 			html += '<tr class="game-row">';
 			for (let x = 0; x < this.matrix.width; x++) {
-				block = this.matrix.getBlock({y, x});
+				block = this.matrix.getBlock({ y, x });
 				html += `<td class="game-cell ${danger} ${getType(block)}">`;
 				html += block;
-				html += '</td>';
+				html += "</td>";
 			}
-			html += '</tr>';
+			html += "</tr>";
 		}
-		html += '</table>';
-		if (this.isGameOver())
-			html += '<p id="game-over">Game over!</h1>'
+		html += "</table>";
+		if (this.isGameOver()) {
+			gameOverModal.style = "display: flex;";
+		}
 		return html;
 	}
 }
