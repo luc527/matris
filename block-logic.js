945e5c5142f7ed2fad68a68ecca209966e88132e
Lucas M. Schwambach <48414967+lucas427@users.noreply.github.com>
2020-09-12 23:47:42 -0300

    game logic changes
    = block removed (not interesting and hard to form expressions with)
    now 8x8 instead of 9x8 (game may take too long)

--- block-logic.js
+++ block-logic.js
@@ -3,7 +3,7 @@
 */
 
 const numbers = ['1','2','3','4','5','6','7','8','9','0'];
-const comparisions = ['=','!=','>','<','>=','<='];
+const comparisions = ['!=','>','<','>=','<='];
 const blocks = [...numbers, ...comparisions];
 
 function isNumber(block) {
