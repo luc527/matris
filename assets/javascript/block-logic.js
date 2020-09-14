e9b2b664729b410f321020773ec343bbdba753fd
Mateus Brandt <mateus.cruz@effecti.com.br>
2020-09-14 11:28:35 -0300

    typo comparision fixed + responsiveness

--- assets/javascript/block-logic.js
+++ assets/javascript/block-logic.js
@@ -2,43 +2,43 @@
    helpers.js (in randomBlock)
 */
 
-const numbers = ['1','2','3','4','5','6','7','8','9','0'];
-const comparisions = ['!=','>','<','>=','<='];
-const blocks = [...numbers, ...comparisions];
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const comparisons = ["!=", ">", "<", ">=", "<="];
+const blocks = [...numbers, ...comparisons];
 
 function isNumber(block) {
 	return numbers.includes(block);
 }
 
-function isComparision(block) {
-	return comparisions.includes(block);
+function isComparison(block) {
+	return comparisons.includes(block);
 }
 
 function getType(block) {
-	if (isNumber(block)) return 'number';
-	if (isComparision(block)) return 'comparision';
-	if (block == ' ') return 'empty';
-	return 'invalid';
+	if (isNumber(block)) return "number";
+	if (isComparison(block)) return "comparison";
+	if (block == " ") return "empty";
+	return "invalid";
 }
 
 function randomBlock() {
 	return randomElement(blocks);
-	/* //50/50 chance of comparision or number
-	return randomElement(Math.random() <= 0.5 ? comparisions : numbers); */
+	/* //50/50 chance of comparison or number
+	return randomElement(Math.random() <= 0.5 ? comparisons : numbers); */
 }
 
 const operatorFunction = {
-	'>':  (l,r) => l >  r,
-	'<':  (l,r) => l <  r,
-	'>=': (l,r) => l >= r,
-	'<=': (l,r) => l <= r,
-	'=':  (l,r) => l == r,
-	'!=': (l,r) => l != r,
+	">": (l, r) => l > r,
+	"<": (l, r) => l < r,
+	">=": (l, r) => l >= r,
+	"<=": (l, r) => l <= r,
+	"=": (l, r) => l == r,
+	"!=": (l, r) => l != r,
 };
 
 function isTrueExpression(leftOperand, operator, rightOperand) {
-	const valid = isNumber(leftOperand) && isComparision(operator) && isNumber(rightOperand);
-	if (!valid)
-		return false;
+	const valid =
+		isNumber(leftOperand) && isComparison(operator) && isNumber(rightOperand);
+	if (!valid) return false;
 	return operatorFunction[operator](Number(leftOperand), Number(rightOperand));
 }
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,28 +3,28 @@
 */
 
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-const comparisions = ["!=", ">", "<", ">=", "<="];
-const blocks = [...numbers, ...comparisions];
+const comparisons = ["!=", ">", "<", ">=", "<="];
+const blocks = [...numbers, ...comparisons];
 
 function isNumber(block) {
 	return numbers.includes(block);
 }
 
-function isComparision(block) {
-	return comparisions.includes(block);
+function isComparison(block) {
+	return comparisons.includes(block);
 }
 
 function getType(block) {
 	if (isNumber(block)) return "number";
-	if (isComparision(block)) return "comparision";
+	if (isComparison(block)) return "comparison";
 	if (block == " ") return "empty";
 	return "invalid";
 }
 
 function randomBlock() {
 	return randomElement(blocks);
-	/* //50/50 chance of comparision or number
-	return randomElement(Math.random() <= 0.5 ? comparisions : numbers); */
+	/* //50/50 chance of comparison or number
+	return randomElement(Math.random() <= 0.5 ? comparisons : numbers); */
 }
 
 const operatorFunction = {
@@ -38,7 +38,7 @@ const operatorFunction = {
 
 function isTrueExpression(leftOperand, operator, rightOperand) {
 	const valid =
-		isNumber(leftOperand) && isComparision(operator) && isNumber(rightOperand);
+		isNumber(leftOperand) && isComparison(operator) && isNumber(rightOperand);
 	if (!valid) return false;
 	return operatorFunction[operator](Number(leftOperand), Number(rightOperand));
 }
