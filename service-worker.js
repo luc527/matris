b1b9172249752ffd3ac99f3e24f5ab40169b51ee
Mateus Brandt <mateus.cruz@effecti.com.br>
2020-09-13 21:20:16 -0300

    offline.html fix

--- service-worker.js
+++ service-worker.js
@@ -65,7 +65,7 @@ this.addEventListener("fetch", (event) => {
 				return response || fetch(event.request);
 			})
 			.catch(() => {
-				return caches.match("index.html");
+				return caches.match("offline.html");
 			})
 	);
 });
